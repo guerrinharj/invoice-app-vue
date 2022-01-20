4ad8ccb548342845557b09ad5b809a0f1fbb04b7
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-01-20 15:01:58 -0300

    did store boilerplate

---
+++ src/store.js
@@ -0,0 +1,21 @@
+//import axios from "axios";
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {
+
+    }
+  },
+  getters: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
+
+export default store
