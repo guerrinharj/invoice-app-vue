ad5e3228b93e9357c955f6a04ccb5758a30aabdc
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-02-01 18:26:44 -0300

    created list item and list element

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
 
 <the-sidebar> </the-sidebar>
 
+<main>
+  <the-list> </the-list>
+</main>
+
 </template>
 
 <script>
 
 import TheSidebar from './components/TheSidebar.vue'
+import TheList from './components/TheList.vue'
 
 export default {
   name: 'App',
   components: {
-    TheSidebar
+    TheSidebar,
+    TheList
   }
 }
 </script>
@@ -20,6 +26,14 @@ export default {
 
 body {
   margin: 0;
+  background-color: #F8F8FB
+}
+
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
 }
 
 </style>
---
+++ src/components/TheList.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <list-item> </list-item>
+  </section>
+</template>
+
+<script>
+
+import ListItem from './ListItem.vue'
+
+  export default {
+    components: { ListItem }
+  }
+
+</script>
+
+<style scoped>
+</style>
