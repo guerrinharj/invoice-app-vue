d68f972272fd4c7eb9bd410b95c3045493adb374
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2022-02-07 18:38:51 -0300

    added item form

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,37 +1,70 @@
 <template>
 <section>
-   <form @submit.prevent="submitForm">
-   <h2> Create invoice</h2>
-   <p> Bill From </p>
-    <label for="invoice-sender-address"> Street Address: </label>
-    <input name="invoice-sender-address" type="text"/>
-    <label for="invoice-sender-city"> City </label>
-    <input name="invoice-sender-city" type="text">
-    <label for="invoice-sender-postcode"> Post Code </label>
-    <input name="invoice-sender-postcode" type="text">
+    <form @submit.prevent="submitForm">
+    <h2> Create invoice</h2>
+    <p> Bill From </p>
+    <div>
+      <label for="invoice-sender-address"> Street Address: </label>
+      <input name="invoice-sender-address" type="text"/>
+    </div>
+
+    <div>
+      <label for="invoice-sender-city"> City </label>
+      <input name="invoice-sender-city" type="text">
+      <label for="invoice-sender-postcode"> Post Code </label>
+      <input name="invoice-sender-postcode" type="text">
+    </div>
     <p> Bill To </p>
-    <label for="invoice-client-name"> Client Name </label>
-    <input name="invoice-client-name" type="text"/>
-    <label for="invoice-client-email"> Client Email </label>
-    <input name="invoice-client-email" type="text"/>
-    <label for="invoice-client-address"> Street Address </label>
-    <input name="invoice-client-address" type="text"/>
-    <label for="invoice-client-city"> City </label>
-    <input name="invoice-client-city" type="text"/>
-    <label for="invoice-client-postcode"> Post Code </label>
-    <input name="invoice-client-postcode" type="text"/>
-    <label for="invoice-client-country"> Country </label>
-    <input name="invoice-client-country" type="text"/>
-    <label for="invoice-client-country"> Country </label>
-    <input name="invoice-client-country" type="text"/>
-    <label for="invoice-date"> Date </label>
-    <Datepicker v-model="date"></Datepicker>
-    <label for="invoice-payment-terms"> Terms </label>
-    <select name="terms">
-      <option v-for="term in paymentTerms" :key="term" :value="term"> {{ term }} </option>
-    </select>
-    <label for="invoice-description"> Description </label>
-    <input name="invoice-description" type="text"/>
+
+    <div>
+      <label for="invoice-client-name"> Client Name </label>
+      <input name="invoice-client-name" type="text"/>
+    </div>
+
+    <div>
+      <label for="invoice-client-email"> Client Email </label>
+      <input name="invoice-client-email" type="text"/>
+    </div>
+
+    <div>
+      <label for="invoice-client-address"> Street Address </label>
+      <input name="invoice-client-address" type="text"/>
+    </div>
+
+    <div>
+      <label for="invoice-client-city"> City </label>
+      <input name="invoice-client-city" type="text"/>
+      <label for="invoice-client-postcode"> Post Code </label>
+      <input name="invoice-client-postcode" type="text"/>
+      <label for="invoice-client-country"> Country </label>
+      <input name="invoice-client-country" type="text"/>
+    </div>
+
+    <div>
+      <label for="invoice-date"> Date </label>
+      <Datepicker v-model="date"></Datepicker>
+      <label for="invoice-payment-terms"> Terms </label>
+      <select name="terms">
+        <option v-for="term in paymentTerms" :key="term" :value="term"> {{ term }} </option>
+      </select>
+    </div>
+
+    <div>
+      <label for="invoice-description"> Description </label>
+      <input name="invoice-description" type="text"/>
+    </div>
+
+    <h3> Item List </h3>
+
+    <div>
+      <label for="invoice-item-name"> Item Name </label>
+      <input name="invoice-item-name" type="text"/>
+      <label for="invoice-item-name"> Qty </label>
+      <input name="invoice-item-name" type="number"/>
+      <label for="invoice-item-name"> Price </label>
+      <input name="invoice-item-name" type="text"/>
+    </div>
+
     <button> Submit </button>
    </form>
 </section>
@@ -62,4 +95,7 @@ export default {
 </script>
 
 <style scoped>
+
+
+
 </style>
